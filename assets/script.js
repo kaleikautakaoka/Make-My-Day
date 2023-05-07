e9dc0f59ec9a532d55fa30143066a032dcca95f0
kaleikautakaoka <107520051+kaleikautakaoka@users.noreply.github.com>
2023-05-06 19:27:18 -0600

    dynamically created hour blocks and comments

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,143 @@
-$(document).ready(function () {
-  let dateDiv = $("#currentDate");
-  let dateApi = dayjs().format("MM/DD/YYYY");
-  let currentTime = dayjs().format("h:mm:a");
-  let timeDiv = $("#currentTime");
+//Vars for date, time, and divs
+const a = dayjs();
+let currentdateDiv = $("#currentDate");
+let currenttimeDiv = $("#currentTime");
+// let dateDiv = $("#currentDate");
+// let currenttimeDiv = $("#currentTime");
+// let dateDiv = $("#currentDate");
+// let currenttimeDiv = $("#currentTime");
+let dateApi = a.format("MM/DD/YYYY");
+const b = a.add(8, "hour");
+console.log(b);
+let currentTime = a.format("h:mm:a");
 
-  console.log(dateApi);
-  console.info("ready");
-  dateDiv.text(dateApi);
-  timeDiv.text(currentTime);
+$(document).ready(function () {
+
+  //Current Time
+  currentdateDiv.text(dateApi);
+  currenttimeDiv.text(currentTime);
+
+  //Dynamically created time blocks
+  $("#presentHourDiv").append(` <div id="hour-9" class="row time-block present">
+        <div class="col-2 col-md-1 hour text-center py-3">9 AM</div>
+        <textarea class="col-8 col-md-10 description" id="text" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>`);
+
+  //Future Time
+  $(
+    "#futureTimeDiv"
+  ).append(`      <div id="hour-10" class="row time-block future">
+        <div class="col-2 col-md-1 hour text-center py-3">10 AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div> `);
+
+  //Past Time
+  $(
+    "#pastTimeDiv"
+  ).append(`        <div id="hour-11" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">11 AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-9" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">12 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-10" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">1 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-11" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">2 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-9" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">3 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-10" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">4 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div>
+
+      <div id="hour-11" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">5 PM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save" value="form">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-save-fill" viewBox="0 0 16 16">
+            <path d="M8.5 1.5A1.5 1.5 0 0 1 10 0h4a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h6c-.314.418-.5.937-.5 1.5v7.793L4.854 6.646a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l3.5-3.5a.5.5 0 0 0-.708-.708L8.5 9.293V1.5z"/>
+          </svg>
+        </button>
+      </div> `);
+
 });
 
-// $(".btn").on("click", saveEvent) {
-  
-//   function saveEvent(e) {
-//     e.preventDefault();
+let createdEvent = $(document).on("click", ".btn", function (e) {
+  e.preventDefault();
 
-//     // $("button").click(function () {
-//     // $(".current").css("color", "orange");
+  // const eventPast =
+  // console.log(userInput);
+  // console.log(event.target);
+  // function saveEvent(e) {
 
-//     // $(".current").addClass("present");
-//     // $(".future").addClass("future");
-//     // $(".past").addClass("past");
-    
-//     // $(".current").removeClass("present");
-//     // $(".future").removeClass("future");
-//     // $(".past").removeClass("past");
-    
-//     // });
+  // $("button").click(function () {
+  // $(".current").css("color", "orange");
 
-// $("textarea").keypress(function (event) {
-//    console.log(event.key);
-//  });
-      
-//   };
-//   };
+  // $(".current").addClass("present");
+  // $(".future").addClass("future");
+  // $(".past").addClass("past");
+
+  // $(".current").removeClass("present");
+  // $(".future").removeClass("future");
+  // $(".past").removeClass("past");
+
+  // });
+
+  // $("textarea").keypress(function (event) {
+  //    console.log(event.key);
+  //  });
+});
